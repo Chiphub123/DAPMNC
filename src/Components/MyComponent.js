b8da7c72886c7e7279ca18e6042232cb7ac5340d
Tram <buingoctram872@gmail.com>
2023-11-03 15:29:40 +0700

    bai3

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -2,12 +2,16 @@
 // function component
 import React from "react"
 class MyComponent extends React.Component {
+    state = {
+        name: "Ngoc Tram",
+        age: 20,
+        address: "Huong Lo 3"
+    };
     //JSX chỉ trả về 1 phần tử
     render() {
         return (
             <div>
-                my first component
-                {Math.random()}
+                My name is {this.state.name} and i'm from {this.state.address}
             </div>
         );
     }
